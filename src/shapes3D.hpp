1114834889c451c63324f556dcb7d943a07447aa
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-04-24 01:42:56 -0400

    Initial shape files and write out header

---
+++ src/shapes3D.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+class shape;
+class box;
+class sphere;
+class torus;
+
+#include <tuple>
+#include "matrix.hpp"
+
+class shape {
+    public:
+        virtual void add(edge_matrix& e) const = 0;
+};
+
+class box : public shape {
+    private:
+        std::tuple<double, double, double> m_frontlefttop;
+        double m_width, m_height, m_depth;
+    public:
+        box(const std::tuple<double, double, double>& m_frontlefttop, double width, double height, double depth);
+        void add(edge_matrix& e) const;
+};
+
+class sphere : public shape {
+    private:
+        std::tuple<double, double, double> m_center;
+        double m_radius;
+    public:
+        sphere(const std::tuple<double, double, double>& center, double radius);
+        void add(edge_matrix& e) const;
+};
+
+class torus : public shape {
+    private:
+        std::tuple<double, double, double> m_center;
+        double m_dist_radius, m_cross_radius;
+    public:
+        torus(const std::tuple<double, double, double>& center, double m_dist_radius, double m_cross_radius);
+        void add(edge_matrix& e) const;
+};
