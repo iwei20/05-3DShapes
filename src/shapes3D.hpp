2dd1f075108a6e1f3f9a9b5487b62f551c150e76
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-04-25 00:24:17 -0400

    Add documentation for shapes3D

--- src/shapes3D.hpp
+++ src/shapes3D.hpp
@@ -13,7 +13,13 @@ class box {
         std::tuple<double, double, double> m_lefttopfront;
         double m_width, m_height, m_depth;
     public:
+        /**
+         * Constructs a box. The given coordinate is used as the coordinate of the left, top and front corner, and the given dimensions are used.
+         **/
         box(const std::tuple<double, double, double>& lefttopfront, double width, double height, double depth);
+        /**
+         * Adds all the lines of this box to an edge matrix.
+         **/
         void add_to(edge_matrix& e) const;
 };
 
@@ -24,9 +30,12 @@ class sphere {
     public:
         sphere(const std::tuple<double, double, double>& center, double radius);
         /**
-         * Adds a sphere to the edge matrix, with [2 * ppsc] semicircles and [ppsc] points per semicircle.
+         * Adds all the points from get_points() into an edge matrix.
          */
         void add_to(edge_matrix& e, int ppsc) const;
+        /**
+         * Gets points on the surface of a sphere, by rotating semicircles to get [2 * ppsc] semicircles and [ppsc] points per semicircle.
+         */
         std::vector<std::tuple<double, double, double>> get_points(int ppsc) const;
 };
 
@@ -36,10 +45,12 @@ class torus {
         double m_dist_radius, m_cross_radius;
     public:
         torus(const std::tuple<double, double, double>& center, double dist_radius, double cross_radius);
-
         /**
-         * Adds a torus to the edge matrix, with [spt] circles and [ppslice] points per circle.
+         * Adds all the points from get_points() into an edge matrix.
          */
         void add_to(edge_matrix& e, int spt, int ppslice) const;
+        /**
+         * Gets points on the surface of a torus, by rotating circles to get [spt] circles and [ppsc] points per circle.
+         */
         std::vector<std::tuple<double, double, double>> get_points(int spt, int ppslice) const;
 };
